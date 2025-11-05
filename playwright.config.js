09aabe7cf7f0efd2e14f62079528594192d2533b
datdev2409 <datdev2409@gmail.com>
2025-11-05 10:01:57 +0700

    integrate with CI

--- playwright.config.js
+++ playwright.config.js
@@ -41,10 +41,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'make live',
-    url: 'http://localhost:9000',
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //   command: 'make live',
+  //   url: 'http://localhost:9000',
+  //   reuseExistingServer: !process.env.CI,
+  // },
 });
 
